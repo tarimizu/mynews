a04956847c773aabbdc442ae2ad1468131faf76c
”taro <tarimizucee@gmail.com>
2019-10-02 02:24:33 +0000

    kadai

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -7,10 +7,9 @@
          <meta charset="utf-8">
          <meta http-equiv="X-UA-Compatible" content="IE=edge">
          <meta name="viewport" content="width=device-width,initial-scale=1">
-         <title>My Profile!!!</title>
+         <title>My Profile!</title>
     </head>
     <body>
-        <h1>My Profile 紹介!!!</h1>
+        <h1>My Profile 紹介</h1>
     </body>
 </html>
-
