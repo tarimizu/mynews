941a9aaf8e6aee392f9bf855cfd6aecf8d56ceff
”taro <tarimizucee@gmail.com>
2019-11-07 02:19:05 +0000

    kadai

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -17,28 +17,28 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="name">名前</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('name',$profile_form->name }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name',$profile_form->name) }}">
                         </div>
                     </div>
                     
                     <div class="form-group row">
                         <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="gender" rows="20">{{ $profile_form->gender }}</textarea>
+                            <textarea class="form-control" name="gender" rows="20">{{ old('gender',$profile_form->gender) }}</textarea>
                         </div>
                     </div>
                     
                     <div class="form-group row">
                         <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="hobby" rows="20">{{ $profile_form->hobby }}</textarea>
+                            <textarea class="form-control" name="hobby" rows="20">{{ old('hobby',$profile_form->hobby) }}</textarea>
                         </div>
                     </div>
                     
                 　　<div class="form-group row">
                         <label class="col-md-2" for="introduction">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction',$profile_form->introduction) }}</textarea>
                         </div>
                     </div>
                             
